3afaac013642dd5044b564761cb2bed70b06d99b
daniellichotti <danielclichotti@gmail.com>
2025-06-05 18:50:48 -0300

    sending classes files

---
+++ aula8/classesEObjetos2.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(name, email, birthDate) {
+    this.name = name
+    this.email = email
+    this.birthDate = birthDate
+  }
+
+  sendEmail() {
+    console.log('email enviado de', this.name, 'com a data de nascimento', this.birthDate, 'do endereço', this.email)
+  }
+}
+
+const newUser = new User('Caio', 'caio@yahoo.com', '07/11/2002')
+
+console.log(newUser.name, newUser.birthDate, newUser.email)
+newUser.sendEmail()
