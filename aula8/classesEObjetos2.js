d7676df3d18193a0b4ec2326919eea3a90ed96f2
daniellichotti <danielclichotti@gmail.com>
2025-06-06 12:03:20 -0300

    sending classes

--- aula8/classesEObjetos2.js
+++ aula8/classesEObjetos2.js
@@ -16,3 +16,65 @@ const newUser = new User('Caio', 'caio@yahoo.com', '07/11/2002')
 
 console.log(newUser.name, newUser.birthDate, newUser.email)
 newUser.sendEmail()
+
+//Método estático
+class UserStatic {
+  constructor(message) {
+    this.message = message
+  }
+
+  static showMessage(message) {
+    console.log(message)
+  }
+}
+
+UserStatic.showMessage('ola mundo')
+
+//Herança
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+
+  makeNoise() {
+    console.log('Algum barulho de animal')
+  }
+}
+
+const newAnimal = new Animal('Dog')
+console.log(newAnimal.name)
+newAnimal.makeNoise()
+console.clear()
+
+class Dog extends Animal {
+  constructor(dogName, race, color) {
+    super(dogName)
+    this.race = race
+    this.color = color
+  }
+
+  makeNoise() {
+    console.log('Au Au Au')
+  }
+}
+
+const newDog = new Dog('Cachorro', 'Pastor Alemão', 'Preto')
+console.log('O animal é um', newDog.name, 'da raça', newDog.race, 'da cor', newDog.color)
+newDog.makeNoise()
+
+class Cat extends Animal {
+  constructor(race, color) {
+    super('Gato')
+    this.race = race
+    this.color = color
+  }
+
+  makeNoise() {
+    console.log('Miau Miau Miau')
+  }
+}
+
+const newCat = new Cat('Siamês', 'Amarelo')
+console.log('O animal é um', newCat.name, 'da raça', newCat.race, 'da cor', newCat.color)
+newCat.makeNoise()
+
