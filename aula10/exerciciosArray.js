3ddc0aecc9de7f32d8ed8c8a6cf131d7d1ae97ce
daniellichotti <danielclichotti@gmail.com>
2025-06-10 22:52:27 -0300

    sending new codes

--- aula10/exerciciosArray.js
+++ aula10/exerciciosArray.js
@@ -17,3 +17,56 @@ console.log(soma)
   Contar quantos elementos são maiores que 20
   Use um loop e condição para contar quantos números são maiores que 20.
 */
+
+let contador = 0
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 20) {
+    contador++ // conta +1 sempre que achar um item maior que 20
+  }
+}
+console.log(contador)
+
+/*
+Filtrar pessoas maiores de idade
+Crie uma função que recebe uma lista de objetos pessoa com nome e idade, e retorna apenas as maiores de 18 anos.
+*/
+const users = [
+  { name: 'André', age: 20 },
+  { name: 'Vinícius', age: 22 },
+  { name: 'José', age: 15 },
+  { name: 'Lucas', age: 17 }
+]
+
+users.forEach(user => {
+  if (user.age >= 18) {
+    console.log(user)
+  }
+})
+
+users.filter(user => user.age >= 18).forEach(user => console.log(user))
+
+/* 
+Criar um novo array com nomes em maiúsculo
+Dado um array de strings, retorne um novo array com os nomes em letras maiúsculas.
+usar .toUpperCase() para string ficar maiúscula
+*/
+const nameList = ['Moisés', 'Leonardo', 'João', 'Glauco', 'Rachel']
+
+console.log(nameList.map(name => name.toUpperCase()))
+console.clear()
+
+/* 
+Calcular média de notas
+Crie uma função que recebe um array de notas e retorne a média. Se a média for maior que 7, retorne "Aprovado", senão "Reprovado".
+*/
+
+const scoreList = [7, 6, 7]
+
+function checkScore(list) {
+  let soma = 0
+  list.forEach(score => soma += score) //é o mesmo que soma = soma + score
+  return (soma / list.length) >= 7 ? 'Aprovado' : 'Reprovado'
+}
+console.log(checkScore(scoreList))
+
